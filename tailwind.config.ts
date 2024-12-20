303e2221affe66b5a085c65b48cc98ea8bf2cc80
naeimDz <naeimdz1703@gmail.com>
2024-12-20 21:23:46 +0100

    ...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,9 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		
   		colors: {
+			
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
